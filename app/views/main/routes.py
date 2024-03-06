e3d4b84e16108920360d43bb66af3adf1b3817bb
alexana3004 <116637406+alexana3004@users.noreply.github.com>
2024-03-06 10:41:55 +0200

    Update routes.py
    revision

--- app/views/main/routes.py
+++ app/views/main/routes.py
@@ -15,6 +15,6 @@ def index():
     latest_recipes = db.session.query(
         Recipe, func.count(Review.rating).label('rating_count'), func.avg(Review.rating).label('avg_rating')).filter(
         Recipe.private.is_(False)).outerjoin(
-        Review, Recipe.id == Review.recipe_id).group_by(Recipe.id).order_by(Recipe.id.desc()).limit(7).all()
+        Review, Recipe.id == Review.recipe_id).group_by(Recipe.id).order_by(Recipe.id.desc()).limit(6).all()
 
     return render_template('index.html', recipes_data=latest_recipes)
